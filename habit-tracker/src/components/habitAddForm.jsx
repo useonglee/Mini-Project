aa85c82c14ed6a6d452e0f7daa5d26615e320b75
useonglee <useong0830@naver.com>
2022-03-08 23:33:06 +0900

    updated habit tracker

--- habit-tracker/src/components/habitAddForm.jsx
+++ habit-tracker/src/components/habitAddForm.jsx
@@ -1,29 +1,27 @@
-import React, { memo } from 'react';
+import React, { memo } from "react";
 
-const HabitAddForm = memo ((props) => {
-    const formRef = React.createRef();
-    const inputRef = React.createRef();
+const HabitAddForm = memo((props) => {
+  const formRef = React.createRef();
+  const inputRef = React.createRef();
 
-    const onSubmit = event => {
-        event.preventDefault();
-        const name = inputRef.current.value;
-        name && props.onAdd(name);
-        // inputRef.current.value = '';
-        formRef.current.reset();
-    };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const name = inputRef.current.value;
+    name && props.onAdd(name);
+    formRef.current.reset();
+  };
 
-    return (
-        <div>
-            <form ref={formRef} className="add-form" onSubmit={onSubmit}>
-                <input ref={inputRef} 
-                type="text" 
-                className="add-input" 
-                placeholder="please, enter your habit" 
-                />
-                <button className="add-button">Add</button>
-            </form>
-        </div>
-    );
+  return (
+    <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        className="add-input"
+        placeholder="Habit"
+      />
+      <button className="add-button">Add</button>
+    </form>
+  );
 });
 
 export default HabitAddForm;
--- habit-tracker/src/index.js
+++ habit-tracker/src/index.js
@@ -1,16 +1,15 @@
-import reportWebVitals from './reportWebVitals'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app';
-import SimpleHabit from './components/simpleHabit';
-import '@fortawesome/fontawesome-free/js/all.js';
+import reportWebVitals from "./reportWebVitals";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app";
+import "@fortawesome/fontawesome-free/js/all.js";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
