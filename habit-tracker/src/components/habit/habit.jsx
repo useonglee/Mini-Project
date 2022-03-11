d879eb85fae9d957eecf4bcc4f5b12474195ef46
useonglee <useong0830@naver.com>
2022-03-11 22:31:01 +0900

    Habit component unit test and Add snapshot test

--- habit-tracker/src/components/habit/habit.jsx
+++ habit-tracker/src/components/habit/habit.jsx
@@ -15,18 +15,24 @@ const Habit = memo(({ habit, onIncrement, onDecrement, onDelete }) => {
 
   return (
     <li className="habit">
-      <span className="habit-name">{habit.name}</span>
-      <span data-testid="count" className="habit-count">
+      <span className="habit-name" data-testid="habit-name">
+        {habit.name}
+      </span>
+      <span className="habit-count" data-testid="habit-count">
         {habit.count}
       </span>
       <button
-        data-testid="increment-button"
         className="habit-button habit-increase"
+        title="increase"
         onClick={handleIncrement}
       >
         <i className="fas fa-plus-square"></i>
       </button>
-      <button className="habit-button habit-decrease" onClick={handleDecrement}>
+      <button
+        className="habit-button habit-decrease"
+        title="decrease"
+        onClick={handleDecrement}
+      >
         <i className="fas fa-minus-square"></i>
       </button>
       <button className="habit-button habit-delete" onClick={handleDelete}>
--- habit-tracker/src/components/habit/tests/habit.test.js
+++ habit-tracker/src/components/habit/tests/habit.test.js
@@ -1,40 +1,47 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import renderer from "react-test-renderer";
 
 import Habit from "../../habit/habit";
 
-describe("render navbar", () => {
+describe("habit component", () => {
+  const habit = { name: "Habit", count: 4 };
+  let HabitComponent;
   let handleIncrement;
-  let button;
-  let habit;
-  let count;
+  let handleDecrement;
+  let handleDelete;
 
   beforeAll(() => {
-    habit = {
-      count: 0,
-      id: 1,
-      name: "Reading",
-    };
-
     handleIncrement = jest.fn();
+    handleDecrement = jest.fn();
+    handleDelete = jest.fn();
 
-    render(
+    HabitComponent = (
       <Habit
         habit={habit}
         onIncrement={handleIncrement}
-        onDecrement={jest.fn()}
-        onDelete={jest.fn()}
+        onDecrement={handleDecrement}
+        onDelete={handleDelete}
       />
     );
-
-    button = screen.getByTestId("increment-button");
-    count = screen.getByTestId("count");
   });
 
-  it("calld handleIncrement when button is clicked", () => {
-    userEvent.click(button);
+  it("render habit component", () => {
+    // snapshot testing
+    const component = renderer.create(HabitComponent);
+    expect(component.toJSON()).toMatchSnapshot();
+  });
 
-    expect(handleIncrement).toHaveBeenCalledTimes(1);
+  describe("Button Click", () => {
+    it("calld handleIncrement when button is clicked", () => {
+      render(HabitComponent);
+
+      const button = screen.getByTitle("increase");
+      userEvent.click(button);
+
+      expect(handleIncrement).toHaveBeenCalledTimes(1);
+      expect(handleIncrement).toHaveBeenCalledWith(habit);
+    });
   });
 });
