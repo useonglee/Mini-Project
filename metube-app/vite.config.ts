fc3935a5bc456190bb2d1b8ea90dae96f9a931b3
useonglee <useong0830@naver.com>
2022-03-18 23:59:07 +0900

    feat: use vite react metube mini project

---
+++ metube-app/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+/**
+ * @type {import('vite').UserConfig}
+ */
+
+module.exports = {
+  plugins: [react()],
+  build: {
+    minify: false,
+  },
+};
