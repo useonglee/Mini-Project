bda289e62c46e4e4bcad1180b2d3f4f0bf718476
useonglee <useong0830@naver.com>
2022-03-19 00:40:51 +0900

    fix: change server filename extension

--- metube-app/src/entry-server.tsx
+++ metube-app/src/entry-server.tsx
@@ -3,9 +3,9 @@ import { Location } from "react-router-dom";
 import { StaticRouter } from "react-router-dom/server";
 import App from "./App";
 
-export function render(url, context) {
+export function render(url: Location) {
   return ReactDOMServer.renderToString(
-    <StaticRouter location={url} context={context}>
+    <StaticRouter location={url}>
       <App />
     </StaticRouter>
   );
