59239a4a7aecf90565cf33336096fad483182af5
useonglee <useong0830@naver.com>
2022-03-23 23:14:53 +0900

    feat: Add header component

--- metube-app/src/components/Header/index.tsx
+++ metube-app/src/components/Header/index.tsx
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { memo, useRef, useState } from "react";
+import { useSearchVideos } from "../../hooks/useSearchVideos";
 import * as Styled from "./styled";
 
-function Header() {
-  return <Styled.HeaderContainer>Header</Styled.HeaderContainer>;
+interface Props {
+  setVideos: any;
 }
 
-export default Header;
+function Header({ setVideos }: Props) {
+  const [query, setQuery] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const { data } = useSearchVideos(query, {
+    onSuccess: () => {
+      query && setVideos(data);
+    },
+    onError: (error: any) => {
+      console.log(error);
+    },
+  });
+
+  const handleSeachValue = () => {
+    const value = inputRef.current!.value;
+    setQuery(value);
+  };
+
+  const handelKeyPress = (event: any) => {
+    if (event.key === "Enter") {
+      handleSeachValue();
+    }
+  };
+
+  return (
+    <Styled.HeaderContainer>
+      <Styled.HeaderLogo>
+        <img src="/images/logo.png" alt="logo image" />
+        <h1>MeTube</h1>
+      </Styled.HeaderLogo>
+      <Styled.SearchInput
+        ref={inputRef}
+        type="search"
+        placeholder="Search"
+        onKeyPress={handelKeyPress}
+      />
+      <button></button>
+    </Styled.HeaderContainer>
+  );
+}
+
+export default memo(Header);
--- metube-app/src/components/Header/styled.ts
+++ metube-app/src/components/Header/styled.ts
@@ -2,6 +2,33 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   display: flex;
+  justify-content: center;
   height: 4rem;
+  padding: 0.8rem 2rem;
   border-bottom: 1px solid ${({ theme }) => theme.color.gray200};
+`;
+
+export const HeaderLogo = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 2em;
+
+  img {
+    margin-right: 0.5em;
+  }
+
+  h1 {
+    font-size: 2rem;
+    font-weight: bold;
+  }
+`;
+
+export const SearchInput = styled.input`
+  width: 100%;
+  padding: 0.8rem;
+  font-size: 1.2rem;
+
+  &:focus {
+    border-color: #0093e9;
+  }
 `;
