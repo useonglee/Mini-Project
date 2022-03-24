dce47434c35ef98cad1041462c595e29cea87adc
useonglee <useong0830@naver.com>
2022-03-24 23:01:18 +0900

    feat: Get data in app component

--- metube-app/src/pages/App.tsx
+++ metube-app/src/pages/App.tsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import Header from "../components/Header";
+import Loading from "../components/Loading";
 import VideoList from "../components/VideoList";
+import { useFetchVideos } from "../hooks/useFetchVideos";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  const { data, isLoading } = useFetchVideos({
+    onSuccess: () => {
+      setVideos(data);
+    },
+    onError: (error: any) => {
+      console.log(error);
+    },
+  });
+
+  if (isLoading) return <Loading />;
+
   return (
     <>
-      <Header />
+      <Header setVideos={setVideos} />
       <VideoList />
     </>
   );
