2de3e0af3a21e2ed303cb260342046174124978c
useonglee <useong0830@naver.com>
2022-03-24 22:59:49 +0900

    feat: Use useQuery Add useSearchVideos hook func

---
+++ metube-app/src/hooks/useSearchVideos.tsx
@@ -0,0 +1,22 @@
+import { useQuery } from "react-query";
+import { axiosService } from "../api";
+
+const searchVideos = async (query: string) => {
+  const response = await axiosService.get("search", {
+    params: {
+      part: "snippet",
+      maxResults: 25,
+      type: "video",
+      q: query,
+    },
+  });
+
+  return response?.data.items.map((item: any) => ({
+    ...item,
+    id: item.id.videoId,
+  }));
+};
+
+export const useSearchVideos = (query: string, options: any) => {
+  return useQuery(["search", query], () => searchVideos(query), options);
+};
