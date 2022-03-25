554f714f3637d893e71762359973a35e106e8847
useonglee <useong0830@naver.com>
2022-03-26 00:07:51 +0900

    feat: Add createRoot and browser router

--- metube-app/src/main.tsx
+++ metube-app/src/main.tsx
@@ -1,23 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./style/globalStyle";
 import theme from "./style/theme";
 import App from "./pages/App";
 
+const container = document.getElementById("root")!;
+const root = createRoot(container);
 const queryClient = new QueryClient();
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
